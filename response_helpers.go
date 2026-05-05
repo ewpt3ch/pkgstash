57d871b7dbab324016fca472121aca71c5d06306
Eric Phillips <eric@ewpt3ch.com>
2026-05-05 11:55:10 -0600

    json response helpers

---
+++ response_helpers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, returnVals{Error: msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
