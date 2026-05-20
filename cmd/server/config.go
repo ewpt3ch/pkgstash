b79f77ea420ca47c624f568dae790e010564357c
Eric Phillips <eric@ewpt3ch.com>
2026-05-20 13:27:08 -0600

    fix: used int when needed octal for permission check

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -61,8 +61,8 @@ func (c *Config) loadToken(path string) error {
 		return fmt.Errorf("failed to stat env file: %v", err)
 	}
 
-	if info.Mode().Perm() != 0640 {
-		return fmt.Errorf("env file perms not secure, expected 0600 got %o", info.Mode().Perm())
+	if info.Mode().Perm() != 0o640 {
+		return fmt.Errorf("env file perms not secure, expected 0640 got %o", info.Mode().Perm())
 	}
 	//#nosec G304 -- config is known path
 	data, err := os.ReadFile(path)
